4a352175532cbd267be9481c0d2e93f59970b713
mikail bhana <mikailbhana@mikails-MacBook-Pro.local>
2025-03-03 15:55:14 +0000

    updating

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,15 @@ from flask import Flask
 from flask_cors import CORS
 import sqlite3
 
+# class that returns database connection. have to create cursor when calling this class
 class DatabaseManager:
     def __init__(self, db_name):
         self.db_name = db_name
 
     def create_connection(self):
-        self.conn = sqlite3.connect(self.db_name)
-        self.cursor = self.conn.cursor()
+        conn = sqlite3.connect(self.db_name)
+        return conn
+
 
 
 app = Flask(__name__)
@@ -16,5 +18,4 @@ CORS(app)
 
 
 if __name__ == "__main__":
-    db = DatabaseManager("budget_database.db")
     app.run(debug=True)
