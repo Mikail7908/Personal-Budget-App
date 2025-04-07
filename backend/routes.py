a0095d7b7b7710f603b18d08863d1f946e508f7b
arnavr06 <arnav.ramamurthy@gmail.com>
2025-04-07 18:04:37 +0100

    Adding sqlite connection and setting up blueprint for routes

--- backend/routes.py
+++ backend/routes.py
@@ -12,4 +12,31 @@ def home():
 @app.route("/transactions", methods=["POST"])
 def create_transaction():
     pass
+
+@app.route("/transactions", methods=["GET"])
+def view_all_transactions():
+    pass
+
+@app.route("/transactions/<int:id>", methods=["PUT"])
+def edit_transaction(id):
+    pass
+
+@app.route("/transactions/<int:id>", methods=["DELETE"])
+def delete_transaction(id):
+    pass
     
+@app.route("/budget-goals", methods=["POST"])
+def create_budget_goal():
+    pass
+
+@app.route("/budget-goals", methods=["POST"])
+def view_all_budget_goals():
+    pass
+
+@app.route("/budget-goals/<int:id>", methods=["PUT"])
+def edit_budget_goal(id):
+    pass
+
+@app.route("/budget-goals/<int:id>", methods=["DELETE"])
+def delete_budget_goal(id):
+    pass
