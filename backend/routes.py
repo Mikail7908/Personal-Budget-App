6f943bcd1ed166c72ac1caded3a1924559e6a256
arnavr06 <arnav.ramamurthy@gmail.com>
2025-04-08 14:33:45 +0100

    Adding routing logic for transactions

--- backend/routes.py
+++ backend/routes.py
@@ -1,29 +1,46 @@
 from main import app
 from flask import *
-
-@app.route("/", methods=["POST"])
-def home():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return 'Logged in currently'
-        
+from models import Transaction, Budget
+from datetime import *
 
 @app.route("/transactions", methods=["POST"])
 def create_transaction():
-    pass
+    transaction_data = request.json()
+    new_transaction = Transaction(
+        amount=transaction_data["amount"],
+        description=transaction_data["description"],
+        date=datetime.strptime(transaction_data["date"], "%Y-%m-%d")
+    )
+    new_transaction.save_to_db()
+    return jsonify({
+        "message": "Successfully created new transaction",
+        "transaction_id": new_transaction.id
+    }), 201
 
 @app.route("/transactions", methods=["GET"])
 def view_all_transactions():
-    pass
+    all_transactions = Transaction.fetch_all()
+    transaction_list = [{
+        "transaction_id": transaction.id,
+        "amount": transaction.amount,
+        "description": transaction.description,
+        "date": transaction.date.strftime("%Y-%m-%d")
+    } for transaction in all_transactions]
+    return jsonify(transaction_list), 200
 
 @app.route("/transactions/<int:id>", methods=["PUT"])
-def edit_transaction(id):
-    pass
+def edit_transaction(transaction_id):
+    transaction_data = request.json
+    updated_transaction = Transaction.update(transaction_id, transaction_data)
+    return jsonify({
+        "message": "Successfully updated transaction",
+        "transaction_id": updated_transaction.id
+    }), 200
 
 @app.route("/transactions/<int:id>", methods=["DELETE"])
-def delete_transaction(id):
-    pass
+def delete_transaction(transaction_id):
+    Transaction.delete(transaction_id)
+    return jsonify({"message": "Transaction deleted successfully"}), 200
     
 @app.route("/budget-goals", methods=["POST"])
 def create_budget_goal():
