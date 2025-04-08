839096d5d3847b95dc37e9d34aea55f70b5e8725
mikail bhana <mikailbhana@mikails-MacBook-Pro.local>
2025-04-08 12:15:16 +0100

    removed nav bar outside container so can stretch full width

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,17 +9,19 @@ import './App.css'
 function App() {
   return (
     <Router>
-      <div className="app">
-        <nav>
-          <ul className="nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/transactions">Transactions</Link></li>
-            <li><Link to="/income">Income</Link></li>
-            <li><Link to="/expenses">Expenses</Link></li>
-            <li><Link to="/budget">Budget</Link></li>
-          </ul>
-        </nav>
+      {/* FULL WIDTH NAVBAR OUTSIDE MAIN CONTAINER */}
+      <nav className="nav">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/transactions">Transactions</Link></li>
+          <li><Link to="/income">Income</Link></li>
+          <li><Link to="/expenses">Expenses</Link></li>
+          <li><Link to="/budget">Budget</Link></li>
+        </ul>
+      </nav>
 
+      {/* MAIN PAGE CONTAINER */}
+      <div className="app">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/transactions" element={<Transactions />} />
@@ -32,4 +34,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
