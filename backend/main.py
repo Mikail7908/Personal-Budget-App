8f967e78a8e270072dc74c11c734dec1ccb085db
arnavr06 <arnav.ramamurthy@gmail.com>
2025-04-08 11:47:13 +0100

    Migrating to SQLAlchemy

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
 class DatabaseManager:
@@ -13,8 +14,11 @@ class DatabaseManager:
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "78af91b1d81e440cbe0059d259d52745"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://budget_database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 CORS(app)
 
+db = SQLAlchemy(app)
 
 if __name__ == "__main__":
     db = DatabaseManager("budget_database.db")
