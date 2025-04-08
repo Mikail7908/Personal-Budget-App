c81353295693e4925748e9835d977814cefb4055
mikail bhana <mikailbhana@mikails-MacBook-Pro.local>
2025-04-08 12:06:00 +0100

    added basic text to pages as placeholder and updated styling

---
+++ frontend/src/Pages/Expenses.jsx
@@ -0,0 +1,11 @@
+function Expenses() {
+    return (
+      <div>
+        <h2>Expenses</h2>
+        <p>Track your spending here and categorize your expenses.</p>
+      </div>
+    )
+  }
+  
+  export default Expenses;
+  
---
+++ frontend/src/Pages/Transactions.jsx
@@ -0,0 +1,10 @@
+function Transactions() {
+    return (
+      <div>
+        <h2>Transactions</h2>
+        <p>Here you’ll be able to view and add transactions.</p>
+      </div>
+    )
+  }
+  export default Transactions;
+  
