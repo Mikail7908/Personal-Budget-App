eb9f65865aed03a9a5fc1409292ddd38172fbb82
arnavr06 <arnav.ramamurthy@gmail.com>
2025-04-10 01:17:04 +0100

    Fixing package names, finishing unit test for transaction

--- testing/test_transaction_services.py
+++ testing/test_transaction_services.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
 import unittest
 from datetime import datetime
 from backend.main import app, db
@@ -5,10 +9,11 @@ from backend.models import  Transaction, Budget
 from backend.services.transaction_service import TransactionService
 
 class TestTransactionService(unittest.TestCase):
-    def set_up_db(self):
+    def setUp(self):
         app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
         app.config["TESTING"] = True
-        self.client = app.test_client()
+        self.app_context = app.app_context()
+        self.app_context.push()
         with app.app_context():
             db.create_all()
             self.budget = Budget(
@@ -19,8 +24,9 @@ class TestTransactionService(unittest.TestCase):
             )
         db.session.add(self.budget)
         db.session.commit()
+        self.client = app.test_client()
     
-    def tear_down_db(self):
+    def tearDown(self):
         with app.app_context():
             db.session.remove()
             db.drop_all()
@@ -32,19 +38,39 @@ class TestTransactionService(unittest.TestCase):
                 "description": "Test transaction",
                 "date": "2025-04-10",
                 "type": "expense",
-                "budget_id": self.budget_id
+                "budget_id": self.budget.id
             }
             test_transaction = TransactionService.create_transaction(test_data)
-            original_spent = self.budget.spent_amount
+            original_spent = Budget.query.get(self.budget.id).spent_amount
             update_test_data = {
                 "amount": "300",
                 "description": "Updated test transaction",
                 "date": "2025-04-10",
                 "type": "expense",
-                "budget_id": self.budget_id
+                "budget_id": self.budget.id
             }
             updated_transaction = TransactionService.update_transaction(test_transaction.id, update_test_data)
             updated_budget = Budget.query.get(self.budget.id)
             self.assertEqual(updated_transaction.amount, 300.0)
             self.assertEqual(updated_budget.spent_amount, original_spent - 200.0 + 300.0)
+            
+    def test_delete_transaction_updates_budget(self):
+        with app.app_context():
+            test_data = {
+                "amount": "100",
+                "description": "Testing delete transaction",
+                "date": "2025-04-10",
+                "type": "expense",
+                "budget_id": self.budget.id
+            }
+            test_transaction = TransactionService.create_transaction(test_data)
+            budget_after_creation =  Budget.query.get(self.budget.id)
+            spent_amount_after_creation = budget_after_creation.spent_amount
+            TransactionService.delete_transaction(test_transaction.id)
+            updated_budget = Budget.query.get(self.budget.id)
+            self.assertEqual(updated_budget.spent_amount, spent_amount_after_creation - 100.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
             
